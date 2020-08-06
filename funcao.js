8e70765141c6854decead5c4f3ff07677d11ff46
Alan Vinicius <alan123vinicius@gmail.com>
2020-08-06 14:50:44 -0300

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,9 @@
+function esquerda(){
+    document.querySelector("#ofertas").style.position = "relative";
+    document.querySelector("#ofertas").style.left = "10px";
+}
+
+function direita(){
+    document.querySelector("#ofertas").style.position = "relative";
+    document.querySelector("#ofertas").style.left = "630px";
+}
