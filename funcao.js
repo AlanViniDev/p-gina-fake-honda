42bcc75a10d6bcbc166279c8c6ca6ce2879d4572
Alan Vinicius <alan123vinicius@gmail.com>
2020-08-06 16:35:23 -0300

    Delete funcao.js

--- funcao.js
+++
@@ -1,9 +0,0 @@
-function esquerda(){
-    document.querySelector("#ofertas").style.position = "relative";
-    document.querySelector("#ofertas").style.left = "10px";
-}
-
-function direita(){
-    document.querySelector("#ofertas").style.position = "relative";
-    document.querySelector("#ofertas").style.left = "630px";
-}
